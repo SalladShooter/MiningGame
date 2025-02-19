8bb79db804dc0b8bcf5bd2a470a7335957d0ddd3
dallasthines <dallasthines@gmail.com>
2025-02-19 09:50:12 -0600

    Added Animate Class

---
+++ animate.py
@@ -0,0 +1,40 @@
+import pygame as pg
+from typing import Tuple
+
+class Animate():
+    def __init__(self, spritesheet):
+        self.spritesheet = spritesheet
+        self.last_frame_time = pg.time.get_ticks()
+        self.frame_row = 0
+        self.frame_col = 0
+        self.frame = [1, 1]
+        self.sprite = self.spritesheet.get_sprite((self.frame_row, self.frame_col))
+        
+    def animate(self, frame_duration, from_frames: Tuple[int, int], to_frames: Tuple[int, int]):
+        self.to_frames = [to_frames[0]+1, to_frames[1]+1]
+        self.from_frames = from_frames
+        self.frame_row = self.frame_row + self.from_frames[0] 
+        self.frame_col = self.frame_col + self.from_frames[1]
+        
+        self.frame_duration = frame_duration
+        self.current_time = pg.time.get_ticks()
+
+        if self.current_time - self.last_frame_time > self.frame_duration:
+            self.frame_col += 1
+
+            if self.frame_col >= self.to_frames[0]:
+                self.frame_col = 0
+                self.frame_row += 1
+                
+                if self.frame_row >= self.to_frames[1]:
+                    self.frame_row = self.from_frames[0]
+
+            self.last_frame_time = self.current_time
+
+        self.sprite = self.spritesheet.get_sprite((self.frame_row, self.frame_col))
+        self.frame = [self.frame_col, self.frame_row]
+        
+    def check_frame(self, frames: Tuple[int, int]):
+        if self.frame == [frames[0]-1, frames[1]-1]:
+            return True
+        return False
--- pickaxe.py
+++ pickaxe.py
@@ -1,46 +1,13 @@
 import pygame as pg
 from spritesheet import Spritesheet
 from typing import Tuple
+from animate import Animate
 
-class Pickaxe:
+class Pickaxe(Animate):
     def __init__(self, size_multiplier):
         self.size_multiplier = size_multiplier
-        self.last_frame_time = pg.time.get_ticks()
         self.spritesheet = Spritesheet("images/pickaxe.png", [16, 16], [0, 0], [16 * self.size_multiplier, 16 * self.size_multiplier])
-        self.frame_row = 0
-        self.frame_col = 0
-        self.frame = [1, 1]
-        self.sprite = self.spritesheet.get_sprite((self.frame_row, self.frame_col))
-
-    def animate(self, frame_duration, from_frames: Tuple[int, int], to_frames: Tuple[int, int]):
-        self.to_frames = [to_frames[0]+1,to_frames[1]+1]
-        self.from_frames = from_frames
-        self.frame_row = self.frame_row + self.from_frames[0] 
-        self.frame_col = self.frame_col + self.from_frames[1]
-        
-        self.frame_duration = frame_duration
-        self.current_time = pg.time.get_ticks()
-
-        if self.current_time - self.last_frame_time > self.frame_duration:
-            
-            self.frame_col += 1
-
-            if self.frame_col >= self.to_frames[0]:
-                self.frame_col = 0
-                self.frame_row += 1
-                
-                if self.frame_row >= self.to_frames[1]:
-                    self.frame_row = self.from_frames[0]
-
-            self.last_frame_time = self.current_time
-
-        self.sprite = self.spritesheet.get_sprite((self.frame_row, self.frame_col))
-        self.frame = [self.frame_col, self.frame_row]
-    
-    def check_frame(self, frames: Tuple[int, int]):
-        if self.frame == [frames[0]-1, frames[1]-1]:
-            return True
-        return False
+        super().__init__(self.spritesheet)
 
     def draw(self, screen):
         screen.blit(self.sprite, (screen.get_width() // 2 - ((16 * self.size_multiplier) // 2), screen.get_height() // 2 - ((16 * self.size_multiplier) // 2)))
